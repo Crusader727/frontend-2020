f2aad0df31c9fbc94de62d41467a8d666cf8447c
Crusader727 <rubenhov97@gmail.com>
2020-02-10 22:30:54 +0300

    new stuff

--- tests/lab2.test.js
+++ tests/lab2.test.js
@@ -1,4 +1,4 @@
-const capitalize = require('../lab2/1.js');
+const capitalize = require('../lab1/1.js');
 
 test('capitalize 1', () => {
     expect(capitalize('я вижу солнце')).toBe('Я Вижу Солнце');
