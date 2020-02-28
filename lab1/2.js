e16acdbea17231a5655cc4996875113273c3bc04
Сергей Алехин <alekhin@MacBook-Pro.local>
2020-02-28 20:05:04 +0300

    Выполнил задание по 1 лабораторной работе Алехин

--- lab1/2.js
+++ lab1/2.js
@@ -7,8 +7,18 @@
  * Примеры:
  * '22 и 3.35, -2, 28, а потом 13, может 1.2 и -134' -> {min: -134, max: 28}
  */
+let MinMax = {
+	min: Number.MAX_VALUE,
+	max: Number.MIN_VALUE
+};
+
 function getMinMax(str) {
-    // code here
+	str.split(/\ |,/).map((el) => {
+		let NumEl = Number(el);
+  	  if (NumEl < MinMax.min) {MinMax.min = NumEl;} else
+  	  if (NumEl > MinMax.max) {MinMax.max = NumEl;}
+	});
+return MinMax;
 }
 
 module.exports = getMinMax;
