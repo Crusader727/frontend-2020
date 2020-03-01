4a85a7a6522fe4630b99c1737d0389d10f93f3d7
Vladislav Alpeev <vlad.alpeev@mail.ru>
2020-03-01 16:11:11 +0300

    Выполнил задание по 1 лабораторной работе Алпеев

--- lab1/1.js
+++ lab1/1.js
@@ -6,7 +6,12 @@
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
  */
 function capitalize(str) {
-    // code here
+    const array_str = str.split(' ')
+    let str_return = []
+    for(let i = 0; i < array_str.length; i++){
+        str_return[i] = array_str[i][0].toUpperCase() + array_str[i].slice(1, array_str[i].length)
+    } 
+    return str_return.join(' ')
 }
 
 module.exports = capitalize;
