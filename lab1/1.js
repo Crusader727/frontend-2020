5780cee49fa4ad94e5002adc6c42591ae100375f
elkhimova-irina <elkhimova-irina@mail.ru>
2020-03-04 17:16:23 +0300

    Выполнила задание по 1 лабораторной работе Елхимова

--- lab1/1.js
+++ lab1/1.js
@@ -1,15 +1,15 @@
+
 /**
  * Напишите функцию capitalize(str),
  * которая вернет строку str, в которой каждое слово начинается с заглавной буквы.
  * Примеры:
  * 'я вижу солнце' -> 'Я Вижу Солнце'
  * 'я Вижу солнце' -> 'Я Вижу Солнце'
- /'я Вижу солнце' -> 'Я Вижу Солнце' **/
-
-
-
+ */
 function capitalize(str) {
-    return  str.split (" ").map(word =>  word[0].toUpperCase() + word.slice(1)).join(" ");
-  }
-  
-  capitalize ('я вижу солнце');
+  // code here
+  return  str.split (" ").map(word =>  word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+
+module.exports = capitalize;
+
--- lab1/3.js
+++ lab1/3.js
@@ -1,13 +1,23 @@
+/**
+ * Напишите функцию multiplyArray(arr, n),
+ * на вход подается массив arr, и число n
+ * необходимо вернуть новый массив, в котором все числа в n раз больше исходного
+ * в массиве могут быть не только числа, эти элементы должны соответствовать исходным (не числа не трогать)
+ * Примеры:
+ * [1, 2, 3, 'ddd', {min: 1}, 22, false], 2 -> [2, 4, 6, 'ddd', {min: 1}, 44, false]
+ */
 function rem(item) { 
   if (typeof item === "number") { 
   item *= this; 
   } 
   return item; 
   } 
-  function multiplyArray(arr, n)
-  {
-    
+  
+function multiplyArray(arr, n) {
+  // code here
   console.log(arr.map(rem,n));
-  }
-    
-  multiplyArray([1, 2, 3, 'ddd', {min: 1}, 22, false], 2);
+}
+
+
+module.exports = multiplyArray;
+
