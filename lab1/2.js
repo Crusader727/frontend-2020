a9a5e5720c93c69ed8e7b001953dc16945b17b5a
Людмила Гапчук <lysicik@mail.ru>
2020-03-11 12:00:49 +0300

    Исправлена 2я задача лаб 1 Гапчук

--- lab1/2.js
+++ lab1/2.js
@@ -8,21 +8,8 @@
  * '22 и 3.35, -2, 28, а потом 13, может 1.2 и -134' -> {min: -134, max: 28}
  */
 function getMinMax(str) {
-    let max=-Math.pow(2,53), min=Math.pow(2,53);
-
-let NumericArray = str.split(/[А-Яа-я]+|[A-Za-z]+|[\ -,]+/);
-
-for (let i=0;i<NumericArray.length;i++){
-    for (let j=i;j<NumericArray.length;j++){
-    if (NumericArray[j]==='') NumericArray.splice(j, 1);
-    else{ 
-            NumericArray[j] = +(NumericArray[j]);
-            if (NumericArray[j]>max) max=NumericArray[j];
-            if (NumericArray[j]<min) min=NumericArray[j];
-        }
-        }
-    }
-    return {min: +min, max: +max};
+	let NumericArray=str.split(/\s*,\s*| /).filter(el => !isNaN(el) );
+	return {min: Math.min(...NumericArray), max: Math.max(...NumericArray)}
 }
 
 module.exports = getMinMax;
